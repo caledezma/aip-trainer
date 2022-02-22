12f10e6215974d3d6cfcaa1969b3a25f24d02c36
caledezma <carlos@nplan.io>
2022-02-22 18:33:56 +0000

    Delete incorrect submission script

--- submit_aip_job.py
+++ submit_aip_job.py
@@ -1,31 +1,52 @@
 """Python script to submit a job to AIP using the python AIP"""
+import argparse
+import os
+import yaml
+
+from dotenv import load_dotenv, find_dotenv
+
 from googleapiclient import discovery, errors
 import logging
 
-training_inputs = {
-    'masterConfig': {
-        'imageUri': 'gcr.io/nplan-scheduling/carlos-aip-test-gpu:wandb'
-    },
-    'scaleTier': 'BASIC_GPU',
-    'args': ['--epochs', '5', '--hidden-layers', '15', '--use-wandb-secret'],
-    'region': 'us-central1',
-    'serviceAccount': 'pipeline@nplan-scheduling.iam.gserviceaccount.com',
-}
+load_dotenv(find_dotenv(usecwd=True))
 
-job_spec = {
-    'jobId': 'train_from_py_1',
-    'trainingInput': training_inputs,
-}
+def main():
+    parser = argparse.ArgumentParser("Submit a training job to AIP")
+    parser.add_argument(
+        "--training-inputs",
+        type=str,
+        desc="Path to a YAML file containing the training args dictionary",
+        required=True,
+    )
+    parser.add_argument(
+        "--job-name",
+        type=str,
+        desc="Unique name of the AIP job",
+        required=True
+    )
+    args = parser.parse_args()
+    with open(args.training_inputs, 'r') as training_input_file:
+        training_inputs = yaml.safe_load(training_input_file)
 
-project_name = 'nplan-scheduling'
-project_id = 'projects/{}'.format(project_name)
-cloudml = discovery.build('ml', 'v1')
+    print(args.job_name)
+    job_spec = {
+        'jobId': args.job_name,
+        'trainingInput': training_inputs,
+    }
+    print(job_spec)
 
-request = cloudml.projects().jobs().create(body=job_spec,parent=project_id)
+    project_name = 'nplan-scheduling'
+    project_id = 'projects/{}'.format(project_name)
+    cloudml = discovery.build('ml', 'v1')
 
-try:
-    response = request.execute()
+    request = cloudml.projects().jobs().create(body=job_spec,parent=project_id)
 
-except errors.HttpError as err:
-    logging.error('There was an error creating the training job. Check the details:')
-    logging.error(err._get_reason())
+    try:
+        response = request.execute()
+
+    except errors.HttpError as err:
+        logging.error('There was an error creating the training job. Check the details:')
+        logging.error(err._get_reason())
+
+if __name__ == "__main__":
+    main()
