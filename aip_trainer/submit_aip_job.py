bd931d2baa9df7cb252988e04e20b5951dc5c40d
caledezma <carlos@nplan.io>
2022-02-22 18:28:54 +0000

    Include AIP submission from python

---
+++ aip_trainer/submit_aip_job.py
@@ -0,0 +1,50 @@
+"""Python script to submit a job to AIP using the python AIP"""
+import argparse
+import os
+import yaml
+
+from dotenv import load_dotenv, find_dotenv
+
+from googleapiclient import discovery, errors
+import logging
+
+load_dotenv(find_dotenv(usecwd=True))
+
+def main():
+    parser = argparse.ArgumentParser("Submit a training job to AIP")
+    parser.add_argument(
+        "--training-inputs",
+        type=str,
+        desc="Path to a YAML file containing the training args dictionary",
+        required=True,
+    )
+    parser.add_argument(
+        "--job-name",
+        type=str,
+        desc="Unique name of the AIP job",
+        required=True
+    )
+    args = parser.parse_args()
+    with open(args.training_inputs, 'r') as training_input_file:
+        training_inputs = yaml.safe_load(training_input_file)
+
+    job_spec = {
+        'jobId': args.job_id,
+        'trainingInput': training_inputs,
+    }
+
+    project_name = 'nplan-scheduling'
+    project_id = 'projects/{}'.format(project_name)
+    cloudml = discovery.build('ml', 'v1')
+
+    request = cloudml.projects().jobs().create(body=job_spec,parent=project_id)
+
+    try:
+        response = request.execute()
+
+    except errors.HttpError as err:
+        logging.error('There was an error creating the training job. Check the details:')
+        logging.error(err._get_reason())
+
+if __name__ == "__main__":
+    main()
